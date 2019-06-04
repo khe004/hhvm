2c269145fb90b6b7bc1f39e3705be8b6b5dc20b2
khe004 <njuhekai@gmail.com>
2019-06-03 19:33:07 -0700

    update

--- test1/.travis.sh
+++ test1/.travis.sh
@@ -4,14 +4,14 @@
 #DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
 php --version
-
+cd /app
 #(
 #  cd $(mktemp -d)
 #  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 #)
-composer install
-#composer require hhvm/hsl hhvm/hhvm-autoload
-#composer require --dev hhvm/hhast hhvm/hacktest facebook/fbexpect
+#composer install
+composer require hhvm/hsl hhvm/hhvm-autoload
+composer require --dev hhvm/hhast hhvm/hacktest facebook/fbexpect
 
 hh_client
 vendor/bin/hacktest tests/
